8b6e6fdffdcf021fda021d186f2766667f8fd489
David Carol <reth27@hotmail.com>
2022-09-12 19:36:57 -0500

    first added.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h1>David Carroll</h1>";
+echo "<p>Check out my <a href=https://www.linkedin.com/in/david-carroll-9b490923a/>LinkedIn</a>";
+echo "<br><br><br>";
+echo "<h3>My work!</h3>";
+echo "<p>I've had to learn a lot in my cyber security classes. Like setting up this website, and using PHP!</p>";
+echo "<br>";
+echo "<p>At my old job, I would make very basic projects with scratch, java, python, and C# as languages. These projects were from my teaching job, where I would teach kids ages 7 to 14 how to code!</p>";
+echo "<br>";
+echo "<p>I have experiance hosting some servers. These are mostly game servers I would host for friends, such as Minecraft, or ARK servers! These passions are what taught me about commands, ports, and being an admin for a server in my younger years!</p>";
+echo "<br>";
+echo "<p>This website is my first attempt at hosting a site of my own. I might try to build upon this basic site as I go, but I don't know yet! Thank you for stoping by and giving this a read.</p>";
